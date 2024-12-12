32ba482423667662d3552fd87260b62ade37d204
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2024-12-12 12:07:35 +0200

    Update params.js

--- src/params.js
+++ src/params.js
@@ -2,6 +2,6 @@ export default class params {
   static keyPush = '328459b8-0405-4796-bbe0-348d682f5e2c'; // ключ OneSignal
   static appID = '6738115749'; // ID додатку в сторі
   static keyApps = 'E4eQAmkzuEWdCxqeqVnJ8Z'; // ключ Appsflyer
-  static targetDate = '2024-12-14'; // дата закінчення відльожки
+  static targetDate = '2024-12-15'; // дата закінчення відльожки
   static bodyLin = 'https://remarkable-exalted-celebration.space/sGKy4RQv'; // тіло лінки з якою будемо працювати
 }
